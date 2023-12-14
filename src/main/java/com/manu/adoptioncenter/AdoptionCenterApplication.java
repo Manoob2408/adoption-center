284a37350b30e5ef2d2d485a61d2fc5fae49cb6e
Emanuela <lopez.emanuela@gmail.com>
2023-12-14 00:03:22 -0300

    Initial commit

---
+++ src/main/java/com/manu/adoptioncenter/AdoptionCenterApplication.java
@@ -0,0 +1,13 @@
+package com.manu.adoptioncenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptionCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptionCenterApplication.class, args);
+	}
+
+}
